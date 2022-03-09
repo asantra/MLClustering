68ba59877dcfb935280904be2320ade7e1184533
asantra <santra.arka@gmail.com>
2022-03-09 13:27:09 +0200

    update from uproot

--- packmanAlgo.py
+++ packmanAlgo.py
@@ -1,7 +1,9 @@
 import os, sys
-from ROOT import *
+#from ROOT import *
 import pprint
 import argparse
+import uproot
+import pandas as pd
 
 
 ### check the x index distance and y index distance. 
@@ -20,31 +22,53 @@ def touching(pixels, cls):
         if(distance(pixels[0]["cellx"], pxix) <= 1 and distance(pixels[0]["celly"], pxiy) <= 1):
             return True;
     return False;
-    
-    
 
 def main():
     
     ### get the input root file name from command line
     parser = argparse.ArgumentParser(description='Code to find seed tracks')
-    parser.add_argument('-l', action="store", dest="inFile", default="dataFile_Signal_e0ppw_3.0_EFieldV10p7p1pyN17Vpercm_Processed_Stave00_Event1.root")
+    parser.add_argument('-l', action="store", dest="inFileName", default="dataFile_Signal_e0ppw_3.0_EFieldV10p7p1pyN17Vpercm_Processed_Stave00_Event1.root")
     args = parser.parse_args()
     
-    ### load the tree from the root file
-    inFile      = TFile(args.inFile, "READ")
-    pixels_tree = inFile.Get("pixels")
+    
     
     ### open an empty dictionary
     dict_cell = {}
     
-    #### take all pixels from the pixels tree, for each bunch crossings
-    for event in pixels_tree:
-        bx = event.bx
+    #############################
+    ##### using pyroot ##########
+    #############################
+    ### load the tree from the root file
+    #inFile      = TFile(args.inFileName, "READ")
+    #pixels_tree = inFile.Get("pixels")
+    
+    ##### take all pixels from the pixels tree, for each bunch crossings
+    #for event in pixels_tree:
+        #bx = event.bx
         
-        dict_cell.setdefault(bx, []).append([{"cellx": event.cellx, "celly": event.celly, "pixId": event.pixId}])
-        
-        #, "pixId": event.pixId, "isSignal": event.isSignal, "charge": event.charge, "tru_p": event.tru_p, "tru_edep": event.tru_edep, "tru_type": event.tru_type, "tru_pdgId": event.tru_pdgId, "tru_trackId": event.tru_trackId, "tru_hit": event.tru_hit, "tru_vertex": event.tru_vertex, "tru_cellx": event.tru_cellx, "tru_celly": event.tru_celly}])
+        #dict_cell.setdefault(bx, []).append([{"cellx": event.cellx, "celly": event.celly, "pixId": event.pixId, "pixId": event.pixId, "isSignal": event.isSignal, "charge": event.charge, "tru_p": event.tru_p, "tru_edep": event.tru_edep, "tru_type": event.tru_type, "tru_pdgId": event.tru_pdgId, "tru_trackId": event.tru_trackId, "tru_hit": event.tru_hit, "tru_vertex": event.tru_vertex, "tru_cellx": event.tru_cellx, "tru_celly": event.tru_celly}])
 
+    
+    #############################
+    ##### using uproot ##########
+    #############################
+    
+    root_file     = uproot.open(args.inFileName)
+    tree          = root_file["pixels"].arrays(root_file["pixels"].keys(), library='np') ##useful as np.array
+    cellx_list    = tree["cellx"]
+    celly_list    = tree["celly"]
+    pixId_list    = tree["pixId"]
+    isSignal_list = tree["isSignal"]
+    
+    
+    ### for now we are interested only on event 1
+    dict_cell[1] = []
+    for i in range(len(cellx_list)):
+        dict_cell[1].append([{"cellx":cellx_list[i], "celly":celly_list[i]}])
+    
+    
+    pprint.pprint(dict_cell)
+    
     
     ### loop over each bx
     for bx in dict_cell:
@@ -66,7 +90,6 @@ def main():
             usedPixel[pixKey] = True
             
             ### loop over all other pixels apart from the pivot pixel
-            #while i < len(dict_cell[bx][ipixels+1:]):
             for other_pixels in dict_cell[bx][ipixels+1:]:
                 #other_pixels = dict_cell[bx][ipixels+1+i]
                 otherPixKey = str(other_pixels[0]["cellx"])+"_"+str(other_pixels[0]["celly"])
@@ -82,25 +105,6 @@ def main():
             
         
         print("for bx: ", bx, " number of clusters: ",len(clusters))
-        
-        #for icls, cls in enumerate(clusters):
-            #### loop over each pixel in each cluster
-            #for ipxls, pxls in enumerate(cls):
-                ##### for each pixel, find out associated tru_ps
-                #print(pxls[0]["pixId"])
-        
-        
-        '''
-        ### loop over each cluster in the bx
-        for icls, cls in enumerate(clusters):
-            ### loop over each pixel in each cluster
-            for ipxls, pxls in enumerate(cls):
-                #### for each pixel, find out associated tru_ps
-                print(pxls[0]["tru_p"])
-                for i in range(len(pxls[0]["tru_p"])):
-                    print("Cluster: ", icls, " Pixel: ", ipxls, " tru_p: ",pxls[0]["tru_p"][i])
-        '''
-        
     
     
 if __name__=="__main__":
